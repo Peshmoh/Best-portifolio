5d7ef277d9069a5e19aebb2818db111d4794978b
peris <perismumbi96@gmail.com>
2024-05-10 16:37:48 +0300

    added a function to make the navbar responsive using js

--- main.js
+++ main.js
@@ -4,3 +4,14 @@
       backSpeed: 150,
       loop: true
     });
+
+    function showSidebar() {
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'flex'
+    
+    }
+
+    function hideSidebar() {
+        const sidebar = document.querySelector('.sidebar')
+        sidebar.style.display = 'none'
+    }
